23d0bd91e53ce350f4b76d835346378696d8fa7f
pavlo <ramison213@gmail.com>
2024-09-03 16:46:14 +0300

    add logger

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { server: srvConfig, data: dataConfig } = require('config');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+const logger = require('./utils/logger')(path.basename(__filename));
 
 const port = srvConfig.port;
 const dbSource = dataConfig.dbSource;
@@ -9,5 +11,5 @@ const app = express();
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Server is now listening on port ${port}`);
+    logger.info(`Server is now listening on port ${port}`);
 })
