1f2bd824c10415099b8d3246e3ee81da80d9293a
pavlo <ramison213@gmail.com>
2024-09-04 19:01:02 +0300

    add register func to user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,15 @@
 const path = require('path');
 const { withAsyncHandler } = require('../errors/errorHandlers');
 const logger = require('../utils/logger')(path.basename(__filename));
+const userService = require('../services/user-service');
 
-async function register(req, res, next) {
+async function register(req, res, _next) {
+    const { email, password } = req.body;
+    const userData = await userService.registration(email, password);
+    // TODO change magic const to variable
+    res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
 
+    return res.json(userData);
 }
 
 async function login(req, res, next) {
@@ -23,7 +29,6 @@ async function refresh(req, res, next) {
 }
 
 async function getUsers(req, res, next) {
-    res.json([1,2,3])
 }
 
 module.exports = {
